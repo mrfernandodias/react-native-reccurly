2137f557b1971e570ed96364ac95505b3e1e8502
Fernando Dias <mrfernandodias@gmail.com>
2026-04-04 10:39:12 -0300

    first commit

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,6 @@
 import "@/global.css";
 import { Stack } from "expo-router";
 
-export const unstable_settings = {
-  initialRouteName: "(tabs)",
-};
-
 export default function RootLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
