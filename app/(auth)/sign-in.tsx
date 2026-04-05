697483a514b25a4a5ac58b3a3a5bf1281bf478ea
Fernando Dias <mrfernandodias@gmail.com>
2026-04-05 12:00:36 -0300

    fix(auth): remove sign-in pathname debug output

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,15 +1,12 @@
 import { ScreenContainer } from "@/components/screen-container";
-import { Link, usePathname } from "expo-router";
+import { Link } from "expo-router";
 import React from "react";
 import { Text } from "react-native";
 
 const SignIn = () => {
-  const pathname = usePathname();
-
   return (
     <ScreenContainer>
       <Text>SignIn</Text>
-      <Text>{pathname}</Text>
       <Link href="/(auth)/sign-up">Create Account</Link>
     </ScreenContainer>
   );
