504c3f7cca1cc8f3c6120372b8c7c86f627ae704
Fernando Dias <mrfernandodias@gmail.com>
2026-04-05 17:39:35 -0300

    chore(build): consolidate Expo config for EAS

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,9 @@ const { withNativewind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Workaround for a Metro validation warning triggered by Expo's generated config.
+if (config.watcher && "unstable_workerThreads" in config.watcher) {
+  delete config.watcher.unstable_workerThreads;
+}
+
 module.exports = withNativewind(config);
